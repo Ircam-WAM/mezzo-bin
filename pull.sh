1005f92a1eae7a29467b9735825e047b65759ed6
Emilie <zawadzki@ircam.fr>
2017-05-17 17:19:23 +0200

    [pull.sh] : add option --restore-db

--- pull.sh
+++ pull.sh
@@ -5,5 +5,8 @@ sudo chown -R $USER var/backup
 git pull
 git submodule foreach --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
 git submodule foreach --recursive 'git pull origin $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
-docker-compose run db /srv/bin/db/restore.sh
+if [ "$1" = "--restore-db" ];
+    then
+    docker-compose run db /srv/bin/db/restore.sh
+fi
 docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
