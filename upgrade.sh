c80532d0defb23e9ea6945929da691668a727012
Emilie <zawadzki@ircam.fr>
2017-06-01 10:34:55 +0200

    [upgrade.sh] : add option --reload-wsgi

--- upgrade.sh
+++ upgrade.sh
@@ -8,4 +8,7 @@ docker-compose run app python /srv/app/manage.py migrate
 docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
 docker-compose run app python /srv/app/manage.py collectstatic --noinput
 docker-compose run app bash /srv/doc/build.sh
-touch app/wsgi.py
+if [ "$1" = "--reload-wsgi" ];
+    then
+    touch app/wsgi.py
+fi
