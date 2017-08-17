efb5a25638849dc06e72a8e774ecbfc5c07e273c
Emilie <zawadzki@ircam.fr>
2017-08-17 17:30:18 +0200

    [db] restore issue typo

--- db/restore.sh
+++ db/restore.sh
@@ -10,8 +10,8 @@ if [ ! -z "$MYSQL" ];
     mysql -h db $MYSQL_DATABASE -u$MYSQL_USER < /srv/backup/mariadb.dump
 elif [ ! -z "$POSTGRES" ];
     then
-    export PGUSER=$POSTGRES_PASSWORD
-    export PGPASSWORD=$DB_ROOT_PASSWORD
+    export PGUSER="postgres"
+    export PGPASSWORD=$POSTGRES_PASSWORD
     export PGDATABASE="postgres"
     pg_restore -c -Fc -hdb /srv/backup/postgres.dump
 fi
