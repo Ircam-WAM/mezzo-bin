164f24ffa40fbf59615ec0906492bc5982d64c21
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-10-26 23:22:19 +0200

    Upadate front build script against new Mezzo arch

--- build_front.sh
+++ build_front.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
+docker-compose run app python /srv/app/manage.py build-front
 docker-compose run app python /srv/app/manage.py collectstatic --no-input
--- pull.sh
+++ pull.sh
@@ -6,16 +6,21 @@ sudo chown -R $USER var/backup
 
 # Update main project
 git pull
+
 # Synchronizes submodules' remote URL configuration setting to the value specified in .gitmodules
 git submodule sync
+
 # Checkout all submodules on right branches specified in .gitmodules, by default the branch is master
 git submodule foreach --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+
 # Pull all submodules on right branches specified in .gitmodules, by default the branch is master
 git submodule foreach --recursive 'git pull origin $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
+
 # Restore database
 if [ "$1" = "--restore-db" ];
     then
     docker-compose run db /srv/bin/db/restore.sh
 fi
+
 # Build front-end
-docker-compose run app bash -c "cd /srv && bower --allow-root install && gulp build"
+docker-compose run app python /srv/app/manage.py build-front
