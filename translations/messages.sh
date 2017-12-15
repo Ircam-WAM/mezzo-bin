5533dfeb1566edb07cef8656509498dc9b2d75cc
Emilie <zawadzki@ircam.fr>
2017-12-15 12:33:25 +0100

    [messages] : only fr

--- translations/messages.sh
+++ translations/messages.sh
@@ -4,6 +4,6 @@ cd /srv
 find $(pwd)  -type d -name 'locale' -print | while read f; do
     cd "$f" && cd ..
     echo $(pwd)
-    django-admin makemessages --all
+    django-admin makemessages -l fr
     django-admin compilemessages
 done
