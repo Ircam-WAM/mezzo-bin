1b83539a29c63d12fb24ff1acfd02d2113ab5b7a
Emilie <zawadzki@ircam.fr>
2017-12-19 17:17:02 +0100

    [Docker] : update build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Always remove intermediate containers
-docker-compose build --force-rm --no-cache
+# Always remove intermediate containers and build app in dev mode
+docker-compose -f docker-compose.yml -f env/dev.yml build --force-rm --no-cache
 # Delete all stopped containers (including data-only containers)
 docker rm $(docker ps -a -q)
 # Delete all 'untagged/dangling' (<none>) images
