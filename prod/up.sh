ffa971f3bd2d4823b685207a31050c0a8ad5d3d7
Emilie <zawadzki@ircam.fr>
2018-03-28 16:15:35 +0200

    [prod] : correcting bash syntax

--- prod/up.sh
+++ prod/up.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-options = ""
-if [ "$1" = "-bg" ]; then
-    options = $options "-d";
+
+options=""
+
+if [ "$1" = "-d" ]; then
+    options=$options"-d";
 fi
+
 docker-compose -f docker-compose.yml -f env/prod.yml up $options
