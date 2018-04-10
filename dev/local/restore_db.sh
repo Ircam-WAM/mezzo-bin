1366e24a0c5d3f34bdae6d5ae4eada122e11855f
Emilie <zawadzki@ircam.fr>
2018-04-10 17:18:52 +0200

    [db] : restore options for mysql

--- dev/local/restore_db.sh
+++ dev/local/restore_db.sh
@@ -6,8 +6,7 @@ echo "Restoring..."
 
 # import database functions of type
 if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
-    export MYSQL_PWD=$MYSQL_ROOT_PASSWORD
-    gunzip < /srv/backup/mariadb.dump.gz | mysql -h db $MYSQL_DATABASE -uroot
+    gunzip < /srv/backup/mariadb.dump.gz | mysql -h db $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
 elif [ ! -z "$POSTGRES_PASSWORD" ]; then
     export PGPASSWORD=$POSTGRES_PASSWORD
     pg_restore -c -hdb -Upostgres -dpostgres  /srv/backup/postgres.dump
