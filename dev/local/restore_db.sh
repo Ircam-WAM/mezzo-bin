03a56833967b3494fd8cc5eadaf4d61527ca8b8f
Raphaël Voyazopoulos <raphael.voyazopoulos@ircam.fr>
2019-02-11 15:50:56 +0100

    Dumps are now timestamped, and restore drops the database before

--- dev/local/restore_db.sh
+++ dev/local/restore_db.sh
@@ -11,7 +11,9 @@ if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
     gunzip < /srv/backup/mariadb.dump.gz | mysql -h db $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
 elif [ ! -z "$POSTGRES_PASSWORD" ]; then
     export PGPASSWORD=$POSTGRES_PASSWORD
-    pg_restore -c -hdb -Upostgres -dpostgres  /srv/backup/postgres.dump
+    dropdb -hdb -Upostgres postgres
+    createdb -hdb -Upostgres -T template0 postgres
+    pg_restore -C -c -hdb -Upostgres -dpostgres /srv/backup/postgres_latest.dump
 fi
 
 echo "Restore done!"
