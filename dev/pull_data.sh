2cb5722d3ebe7450b56ef921f58a6d5df7777f8b
Emilie Zawadzki <emilie.zawadzki@ircam.fr>
2019-03-08 12:07:21 +0100

    [pull_data] : hard reset + chown on media and static

--- dev/pull_data.sh
+++ dev/pull_data.sh
@@ -6,7 +6,9 @@ bin/misc/fix_var_perms.sh
 
 cd "$(dirname "$0")"/../../var/
 
-git pull
+git reset --hard origin/master
+git pull origin master
+sudo chown -R www-data media static
 
 # Restore database
 if [ "$1" = "--restore-db" ]; then
