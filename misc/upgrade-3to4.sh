f3351c25d7b71ff87fc0a857dc10fc600c490c98
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-05-30 14:33:05 +0200

    absolute paths

--- misc/upgrade-3to4.sh
+++ misc/upgrade-3to4.sh
@@ -1,10 +1,15 @@
-./manage.py rename_app organization-core organization_core
-./manage.py rename_app organization-agenda organization_agenda
-./manage.py rename_app organization-formats organization_formats
-./manage.py rename_app organization-job organization_job
-./manage.py rename_app organization-magazine organization_magazine
-./manage.py rename_app organization-media organization_media
-./manage.py rename_app organization-network organization_network
-./manage.py rename_app organization-pages organization_pages
-./manage.py rename_app organization-projects organization_projects
-./manage.py rename_app organization-shop organization_shop
+python /srv/app/manage.py rename_app organization-core organization_core
+python /srv/app/manage.py rename_app organization-agenda organization_agenda
+python /srv/app/manage.py rename_app organization-formats organization_formats
+python /srv/app/manage.py rename_app organization-job organization_job
+python /srv/app/manage.py rename_app organization-magazine organization_magazine
+python /srv/app/manage.py rename_app organization-media organization_media
+python /srv/app/manage.py rename_app organization-network organization_network
+python /srv/app/manage.py rename_app organization-pages organization_pages
+python /srv/app/manage.py rename_app organization-projects organization_projects
+python /srv/app/manage.py rename_app organization-shop organization_shop
+
+python /srv/app/manage.py migrate rest_framework_api_key zero
+python /srv/app/manage.py migrate rest_framework_api_key
+
+python /srv/app/manage.py migrate
